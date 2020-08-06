764129e152cd710da92a500013eb783c7e26af48
Mykola <mathematic204chnu@gmail.com>
2020-08-06 14:20:03 +0300

    уточнення

--- TasksDistribution.php
+++ TasksDistribution.php
@@ -73,12 +73,14 @@ class TasksDistribution
         $this->times = $times;
         $this->max_time = $max_time;
         $this->tasks_n_times = [];
-        for ($i=0; $i<sizeof($tasks); $i++)
-            $this->tasks_n_times += [$this->tasks[$i] => $this->times[$i]];
+        foreach ($this->tasks as $key => $task) {
+            $this->tasks_n_times[] = [$task => $this->times[$key]];
+        }
         $this->time_sum = array_sum($this->times);
-        $this->time = array_sum($this->times)/sizeof($this->instants);
-        $this->result = array_fill(0,sizeof($this->instants),[]);
+        $this->time = array_sum($this->times)/count($this->instants);
+        $this->result = array_fill(0,count($this->instants),[]);
     }
+
     /**
      * @return array
      */
@@ -97,35 +99,33 @@ class TasksDistribution
     public function distribute()
     {
         $T = ceil($this->time);
-        $b = true;  //показує чи є завдання із часом виконання, меншим ніж sizeof($this->instants)
-        $t = array_fill(0,sizeof($this->instants),0); //масив, елементами якого є час виконання завдань відповідного виконавця завдань
+        $b = true;  //показує чи є завдання із часом виконання, меншим ніж count($this->instants)
+        $t = array_fill(0,count($this->instants),0); //масив, елементами якого є час виконання завдань відповідного виконавця завдань
 
-        for ($i = 0; $i < sizeof($this->instants); $i++)    //проходження по всім виконавцям завдань
+        for ($i = 0; $i < count($this->instants); $i++)    //проходження по всім виконавцям завдань
         {
-            while ($t[$i] < $T && sizeof($this->times)>0 && $b)  //перша умова - це поки час усіх завдань менший за середній час, друга - поки є завдання, третя - див комент вище
+            while ($t[$i] < $T && count($this->times)>0 && $b)  //перша умова - це поки час усіх завдань менший за середній час, друга - поки є завдання, третя - див комент вище
             {
                 $j = $this->getNearestNumber($this->times, $T - $t[$i]);  //номер найбільшого елемента, із яким час усіх завдань в виконавці не буде більшим ніж середній час
-                if ($j === -1) //якщо $j === -1, то немає завдання із часом виконання, меншим ніж sizeof($this->instants)
+                if ($j === -1) //якщо $j === -1, то немає завдання із часом виконання, меншим ніж count($this->instants)
                     $b = false;
                 else {
                     $this->result[$i] += [$j => $this->tasks[$this->getNearestNumber($this->times, $T - $t[$i])]];
                     $t[$i] += $this->times[$this->getNearestNumber($this->times, $T - $t[$i])];
-                    unset($this->tasks[$j]);
-                    unset($this->times[$j]);
+                    unset($this->tasks[$j], $this->times[$j]);
                 }
             }
             $b = true;
             ksort($this->result[$i]);
         }
-        $n = sizeof($this->times);
+        $n = count($this->times);
         for ($i = 0; $i < $n; $i++)  //запис тих завдань, що залишилися до тих виконавців,так, щоб завдання із найбільшим часом було у завдання із найманшим часом
         {
             $j = array_search(max($this->times),$this->times);  //номер завдання із найбфльшим часом
             $number_instans = array_search(min($t),$t); //номер виконавця із найменшим сумарним часом
             $this->result[$number_instans] += [$j => $this->tasks[$j]];
             $t[$number_instans] += $this->times[$j];
-            unset($this->tasks[$j]);
-            unset($this->times[$j]);
+            unset($this->tasks[$j], $this->times[$j]);
             ksort($this->result[$number_instans]);
         }
     }
@@ -152,8 +152,8 @@ class TasksDistribution
 
     public function sumResult() //сума часу кожного виконавця
     {
-        $sum = array_fill(0,sizeof($this->instants),0);
-        for ($i = 0; $i < sizeof($this->instants); $i++)
+        $sum = array_fill(0,count($this->instants),0);
+        for ($i = 0; $i < count($this->instants); $i++)
         {
             foreach ($this->result[$i] as $key=>$value)
                 $sum[$i] += $this->tasks_n_times[$value];
